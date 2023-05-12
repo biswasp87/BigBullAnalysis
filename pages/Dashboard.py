7a0a648fe6e412168eb3c999df2ea8f861b8abcd
biswasp87 <76902729+biswasp87@users.noreply.github.com>
2023-05-12 06:28:00 +0530

    Added callback for all three cards

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -5,8 +5,8 @@ import requests
 dash.register_page(
     __name__,
     path='/',
-    title='Home',
-    name='App Details'
+    title='Settings',
+    name='Settings'
 )
 
 from dash import Dash, dcc, html, Input, Output, callback
@@ -25,13 +25,12 @@ master_data = pd.read_csv("gs://biswasp87/last_Master_Data_updated_on.csv")
 watchlist = pd.read_csv("gs://biswasp87/last_watchlist_updated_on.csv")
 scanner = pd.read_csv("gs://biswasp87/last_scanner_updated_on.csv")
 
-
 first_card_content = [
     dbc.CardHeader("Master Data"),
     dbc.CardBody(
         [
             html.H5(className="card-title", id='master_data_heading'),
-            html.P(className="card-text", id='master_data'),
+            html.P(className="card-text", id='master_data_para'),
             dbc.Button("Update Now", color="primary", id='master_data_button'),
         ]
     ),
@@ -40,12 +39,9 @@ second_card_content = [
     dbc.CardHeader("Watchlist"),
     dbc.CardBody(
         [
-            html.H5("Last Updated on", className="card-title"),
-            html.P(
-                "This is some card content that we'll reuse",
-                className="card-text",
-            ),
-            dbc.Button("Update Now", color="primary", href="https://asia-south1-phrasal-fire-373510.cloudfunctions.net/BBA_Update_Scanner_10m_opt_vol"),
+            html.H5(className="card-title", id='watchlist_data_heading'),
+            html.P(className="card-text", id='watchlist_data_para'),
+            dbc.Button("Update Now", color="primary", id='watchlist_data_button'),
         ]
     ),
 ]
@@ -53,12 +49,9 @@ third_card_content = [
     dbc.CardHeader("Scanner"),
     dbc.CardBody(
         [
-            html.H5("Last Updated on", className="card-title"),
-            html.P(
-                "This is some card content that we'll reuse",
-                className="card-text",
-            ),
-            dbc.Button("Update Now", color="primary"),
+            html.H5(className="card-title", id='scanner_data_heading'),
+            html.P(className="card-text", id='scanner_data_para'),
+            dbc.Button("Update Now", color="primary", id='scanner_data_button'),
         ]
     ),
 ]
@@ -78,16 +71,15 @@ content = html.Div(
 )
 
 layout = html.Div([content])
-
 @callback(
     Output(component_id='master_data_heading', component_property='children'),
-    Output(component_id='master_data', component_property='children'),
-    Input(component_id='master_data_button', component_property='n_clicks')
+    Output(component_id='master_data_para', component_property='children'),
+    Input(component_id='master_data_button', component_property='n_clicks'),
 )
-def update_output_div(n):
-    master_data_last_updated_on = master_data['last_data_day'].iloc[-1]
+def update_master_data_card(n):
+    master_last_updated_on = master_data['last_data_day'].iloc[-1]
     if n is None:
-        text_one = "Last Updated on " + master_data_last_updated_on
+        text_one = "Last Updated on " + master_last_updated_on
         text_two = "Click on the Button for Manual Update"
         return text_one, text_two
     elif n == 1:
@@ -105,4 +97,56 @@ def update_output_div(n):
         text_one = ""
         text_two = 'Avoid Updating Multiple Times'
         return text_one, text_two
+@callback(
+    Output(component_id='watchlist_data_heading', component_property='children'),
+    Output(component_id='watchlist_data_para', component_property='children'),
+    Input(component_id='watchlist_data_button', component_property='n_clicks'),
+)
+def update_watchlist_data_card(n):
+    watchlist_last_updated_on = watchlist['last_data_day'].iloc[-1]
+    if n is None:
+        text_one = "Last Updated on " + watchlist_last_updated_on
+        text_two = "Click on the Button for Manual Update"
+        return text_one, text_two
+    elif n == 1:
+        r = requests.post("https://asia-south1-phrasal-fire-373510.cloudfunctions.net/BBA_Update_Watchlist")
+        r_status = r.status_code
+        if r_status == 200:
+            text_one = 'Updated Successfully at ' + str(datetime.now())
+            text_two = ''
+            return text_one, text_two
+        else:
+            text_one = 'Update failed with HTTP error ' + str(r_status)
+            text_two = 'Try Later'
+            return text_one, text_two
+    else:
+        text_one = ""
+        text_two = 'Avoid Updating Multiple Times'
+        return text_one, text_two
 
+@callback(
+    Output(component_id='scanner_data_heading', component_property='children'),
+    Output(component_id='scanner_data_para', component_property='children'),
+    Input(component_id='scanner_data_button', component_property='n_clicks'),
+)
+def update_scanner_data_card(n):
+    scanner_last_updated_on = scanner['last_data_day'].iloc[-1]
+    if n is None:
+        text_one = "Last Updated on " + scanner_last_updated_on
+        text_two = "Click on the Button for Manual Update"
+        return text_one, text_two
+    elif n == 1:
+        r = requests.post("https://asia-south1-phrasal-fire-373510.cloudfunctions.net/BBA_Update_Scanner_10m_opt_vol")
+        r_status = r.status_code
+        if r_status == 200:
+            text_one = 'Updated Successfully at ' + str(datetime.now())
+            text_two = ''
+            return text_one, text_two
+        else:
+            text_one = 'Update failed with HTTP error ' + str(r_status)
+            text_two = 'Try Later'
+            return text_one, text_two
+    else:
+        text_one = ""
+        text_two = 'Avoid Updating Multiple Times'
+        return text_one, text_two
